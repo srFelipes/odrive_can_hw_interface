d90ce8032f52f6bff9d2ac5a240e1b00f5a7495a
srFelipes <fgerman.mena@gmail.com>
2023-12-29 13:52:14 -0300

    first basic implementation

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ odrive_can_hw_interface
 hardware interface of ROS control 2 to odrive via CAN bus
 
 ![Licence](https://img.shields.io/badge/License-GPL-3.0-only-blue.svg)
+
+Pluginlib-Library: odrive_can_hw_interface
+Plugin: odrive_can_hw_interface/OdriveCanHwInterface (hardware_interface::ActuatorInterface)
---
+++ src/odrive_can_hw_interface.cpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2023, Felipe Guajardo
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+#include <vector>
+
+#include "odrive_can_hw_interface/odrive_can_hw_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace odrive_can_hw_interface
+{
+hardware_interface::CallbackReturn OdriveCanHwInterface::on_init(
+  const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::ActuatorInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::SUCCESS;
+  }
+
+  // TODO(anyone): read parameters and initialize the hardware
+  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn OdriveCanHwInterface::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to be ready for read calls and write calls of some interfaces
+
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> OdriveCanHwInterface::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      // TODO(anyone): insert correct interfaces
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
+  }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> OdriveCanHwInterface::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      // TODO(anyone): insert correct interfaces
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+  }
+
+  return command_interfaces;
+}
+
+hardware_interface::CallbackReturn OdriveCanHwInterface::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to receive commands
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn OdriveCanHwInterface::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to stop receiving commands
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::return_type OdriveCanHwInterface::read(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  // TODO(anyone): read robot states
+
+  return hardware_interface::return_type::OK;
+}
+
+hardware_interface::return_type OdriveCanHwInterface::write(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  // TODO(anyone): write robot's commands'
+
+  return hardware_interface::return_type::OK;
+}
+
+}  // namespace odrive_can_hw_interface
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  odrive_can_hw_interface::OdriveCanHwInterface, hardware_interface::ActuatorInterface)
